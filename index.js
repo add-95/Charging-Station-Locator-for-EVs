b40198b0845bec5164d0602498ba093b89e97eec
Atanu Das <atanudas0985@gmail.com>
2023-05-17 15:36:27 +0530

    Adding CSS to home Page

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 const port = 8000;
 
 // use express router
@@ -7,8 +8,9 @@ app.use('/', require('./routes'));
  
 // set up our view engine
 app.set('view engine', 'ejs');
-app.set('views', './views');
+app.set('views', path.join(__dirname,'views'));
 
+app.use(express.static('assets'));
 
 app.listen(port, function(err){
     if(err){
