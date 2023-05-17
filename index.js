8b6af5dd2285126b71756683fa150b7e5acf2ed3
Atanu Das <atanudas0985@gmail.com>
2023-05-17 14:58:23 +0530

    Installing Express and set Routes

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const port = 8000;
+
+// use express router
+app.use('/', require('./routes'));
+
+
+
+app.listen(port, function(err){
+    if(err){
+        console.log(`Error in Running the Server: ${err}`);
+    }
+    console.log(`Server is Running in the port: ${port}`);
+});
