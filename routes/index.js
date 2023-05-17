842ecf70664ad9d409feeeeea469ab4706da7cec
Atanu Das <atanudas0985@gmail.com>
2023-05-17 15:49:02 +0530

    Adding About Page with controller and css

--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,11 @@ const express = require('express');
 
 const router = express.Router();
 const homeController = require('../controllers/home_controller');
+const aboutController = require('../controllers/about_controller');
 console.log("Router Loaded..");
 
 router.get('/', homeController.home);
-
+router.get('/about', aboutController.about);
 
 
 module.exports = router;
