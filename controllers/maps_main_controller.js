fe374666920024f5d8f17cdd72a9a9939e64d771
Atanu Das <atanudas0985@gmail.com>
2023-05-23 12:07:15 +0530

    Removing Basic Map from Our Project

--- controllers/maps_main_controller.js
+++ controllers/maps_main_controller.js
@@ -1,18 +1,18 @@
-module.exports.basic = function(req,res){
+// module.exports.basic = function(req,res){
 
-    return res.render('mapBasic', {
-        title: "Basic Map Page"
-    });
-}
+//     return res.render('mapBasic', {
+//         title: "Basic Map Page"
+//     });
+// }
 module.exports.demo = function(req,res){
     
     return res.render('demoMap', {
-        title: "demo Map"
+        title: "Google-Map api"
     });
 }
 module.exports.myMap = function(req,res){
 
     return res.render('myMap', {
-        title: "my Map"
+        title: "my_Map"
     });
 }
--- routes/mainMap.js
+++ routes/mainMap.js
@@ -3,8 +3,9 @@ const express = require('express');
 const router = express.Router();
 const mainMapController = require('../controllers/maps_main_controller');
 
-router.get('/', mainMapController.basic);
+// router.get('/', mainMapController.basic);
 router.get('/demo', mainMapController.demo);
+router.get('/', mainMapController.myMap);
 router.get('/my-map', mainMapController.myMap);
 
 
